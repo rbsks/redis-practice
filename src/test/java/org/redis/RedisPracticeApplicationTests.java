9f045a7e0f372d2ec9135529b52451ac4c1998db
rbsks <rbsks147@daum.net>
2024-08-25 10:33:27 +0900

    init commit

---
+++ src/test/java/org/redis/RedisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
